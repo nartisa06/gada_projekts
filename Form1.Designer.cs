b9a55d3d2f723f88d3dcdf7e3f1d6efb2d2b5792
Enija Nartiša <en2632@gymn.local>
2024-04-10 11:42:41 +0300

    dizains datu baze

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,21 +35,21 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.b_tabula = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.b_Dzest = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.b_saglabat_db = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(109)))), ((int)(((byte)(112)))));
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(119)))), ((int)(((byte)(116)))));
             this.richTextBox1.Location = new System.Drawing.Point(175, 84);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(180, 159);
@@ -59,6 +59,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
@@ -68,7 +69,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(213, 41);
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(172, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(95, 13);
             this.label2.TabIndex = 2;
@@ -77,15 +80,15 @@
             // 
             // tb_gramatas_nosaukums
             // 
-            this.tb_gramatas_nosaukums.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(109)))), ((int)(((byte)(112)))));
-            this.tb_gramatas_nosaukums.Location = new System.Drawing.Point(49, 84);
+            this.tb_gramatas_nosaukums.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(119)))), ((int)(((byte)(116)))));
+            this.tb_gramatas_nosaukums.Location = new System.Drawing.Point(29, 84);
             this.tb_gramatas_nosaukums.Name = "tb_gramatas_nosaukums";
             this.tb_gramatas_nosaukums.Size = new System.Drawing.Size(100, 20);
             this.tb_gramatas_nosaukums.TabIndex = 3;
             // 
             // richTextBox2
             // 
-            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(109)))), ((int)(((byte)(112)))));
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(119)))), ((int)(((byte)(116)))));
             this.richTextBox2.Location = new System.Drawing.Point(383, 84);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(146, 159);
@@ -95,6 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(399, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(94, 13);
@@ -103,20 +107,21 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(136)))), ((int)(((byte)(115)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 465);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1072, 228);
             this.dataGridView1.TabIndex = 6;
             // 
-            // button1
+            // b_tabula
             // 
-            this.button1.Location = new System.Drawing.Point(29, 427);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Pārskatīt datus";
-            this.button1.UseVisualStyleBackColor = true;
+            this.b_tabula.Location = new System.Drawing.Point(29, 427);
+            this.b_tabula.Name = "b_tabula";
+            this.b_tabula.Size = new System.Drawing.Size(164, 23);
+            this.b_tabula.TabIndex = 7;
+            this.b_tabula.Text = "Pārskatīt datus";
+            this.b_tabula.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -126,18 +131,19 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 8;
             // 
-            // button2
+            // b_Dzest
             // 
-            this.button2.Location = new System.Drawing.Point(626, 409);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(164, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Dzēst kādu no grāmatām";
-            this.button2.UseVisualStyleBackColor = true;
+            this.b_Dzest.Location = new System.Drawing.Point(626, 409);
+            this.b_Dzest.Name = "b_Dzest";
+            this.b_Dzest.Size = new System.Drawing.Size(164, 23);
+            this.b_Dzest.TabIndex = 9;
+            this.b_Dzest.Text = "Dzēst kādu no grāmatām";
+            this.b_Dzest.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(923, 412);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 13);
@@ -147,6 +153,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(584, 51);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(57, 13);
@@ -156,6 +163,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(731, 51);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 13);
@@ -178,14 +186,15 @@
             this.textBox3.Size = new System.Drawing.Size(152, 20);
             this.textBox3.TabIndex = 14;
             // 
-            // button3
+            // b_saglabat_db
             // 
-            this.button3.Location = new System.Drawing.Point(558, 196);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(446, 23);
-            this.button3.TabIndex = 15;
-            this.button3.Text = "Saglabāt datus";
-            this.button3.UseVisualStyleBackColor = true;
+            this.b_saglabat_db.Location = new System.Drawing.Point(558, 196);
+            this.b_saglabat_db.Name = "b_saglabat_db";
+            this.b_saglabat_db.Size = new System.Drawing.Size(446, 23);
+            this.b_saglabat_db.TabIndex = 15;
+            this.b_saglabat_db.Text = "Saglabāt datus";
+            this.b_saglabat_db.UseVisualStyleBackColor = true;
+            this.b_saglabat_db.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -193,15 +202,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(172)))), ((int)(((byte)(157)))));
             this.ClientSize = new System.Drawing.Size(1096, 714);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.b_saglabat_db);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.b_Dzest);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b_tabula);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.richTextBox2);
@@ -226,15 +235,15 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button b_tabula;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button b_Dzest;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button b_saglabat_db;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
 
 namespace EN_dizaina_risinājums_2024
 {
@@ -23,6 +25,25 @@ namespace EN_dizaina_risinājums_2024
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        static SQLiteConnection CreatConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=db_gramatas.db; Version = 3; New = True; Compress = True; ");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
